a5bec02ee26ab3988dd7c94d843af568a6fff189
kirti-singla123 <kirtisingla38231@gmail.com>
2025-08-19 18:17:55 +0530

    whatsapp

--- bookings/models.py
+++ bookings/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Booking(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
@@ -9,6 +7,14 @@ class Booking(models.Model):
     date = models.DateField()
     time = models.TimeField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Automatically prepend +91 if missing (for India)
+        if not self.phone.startswith('+'):
+            # Remove any spaces, dashes, or parentheses
+            digits_only = ''.join(filter(str.isdigit, self.phone))
+            self.phone = f'+91{digits_only}'
+        super().save(*args, **kwargs)
 
     def __str__(self):
         # e.g. "Aisha – Classic Facial on 2025-08-18 14:30"
